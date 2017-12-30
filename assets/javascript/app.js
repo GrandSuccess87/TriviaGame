688f8cb03100bab0528ef2383c8b8fa2667111c9
Quintessa Anderson <quintessaanderson@Quintessas-MacBook-Pro.local>
2017-12-29 22:15:51 -0600

    second commit w/ timer functioning

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,14 @@ var number = 45;
 
 var intervalId;
 
+// var questions = [
+//     {
+//         question: "Lorem Ipsum question1",
+//         choices: ["Answer", "Answer", "Answer",],
+//         answer: 2
+//     },
+// ]
+
 function run() {
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
@@ -24,8 +32,27 @@ function stop() {
 }
 
 
+
+
+// function quizQuestions() {
+    
+//            for (var i = 0; i < questions.length; i++) {
+//                $("#quiz").append(
+//                 "<div class='question'>" + questions[i].question + "</div>" +
+//                 "<form action=''>" +  
+//                "<input type='radio' class='answerChoice'>"+ questions[i].choices[0]+ "</div>" +
+//                 "<input type='radio' class='answerChoice'>"+ questions[i].choices[1]+ "</div>" +
+//                 "<input type='radio' class='answerChoice'>"+ questions[i].choices[2]+ "</div>" + "</form>");
+    
+//             }
+//           }
+
 function nextQuestion() {
     number = 45;
     run();
     decrement();
 }
+
+run();
+
+
